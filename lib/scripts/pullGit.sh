f27cd19cb64fae2814933cf0be76dc61096e098e
Patrick Maximilian Reister <patrickreister@googlemail.com>
2016-04-06 15:15:11 +0200

    Adding support for submodules

--- lib/scripts/pullGit.sh
+++ lib/scripts/pullGit.sh
@@ -4,7 +4,7 @@ printf "#\041/bin/bash\nssh -i \"$HOME/.ssh/id_rsa\" \$1 \$2\n" > $HOME/ssh_wrap
 chmod +x $HOME/ssh_wrapper.sh
 
 cd $HOME
-GIT_SSH=$HOME/ssh_wrapper.sh git clone "$1" $2
+GIT_SSH=$HOME/ssh_wrapper.sh git clone --recursive "$1" $2
 if [ $? -ne 0 ]; then
 	exit 2
 fi
